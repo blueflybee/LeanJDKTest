6cca32417c65500dbb3086bb16b35b21264896cb
shaojun <wusj2017@gmail.com>
2019-05-05 11:11:21 +0800

    StringBuilder学习和测试

--- src/test/java/lang/AbstractStringBuilderTest.java
+++ src/test/java/lang/AbstractStringBuilderTest.java
@@ -4,6 +4,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class AbstractStringBuilderTest {
 
 
@@ -86,6 +88,49 @@ public class AbstractStringBuilderTest {
     int codePointCount = sb.codePointCount(0, sb.length());
     System.out.println("codePointCount = " + codePointCount);
 
+  }
+
+  @Test
+  public void testOffsetByCodePoints() {
+
+    StringBuilder sb = new StringBuilder("abcd𠮷123456");
+    System.out.println("sb = " + sb);
+
+    int offsetByCodePoints = sb.offsetByCodePoints(7, -2);
+    System.out.println("offsetByCodePoints = " + offsetByCodePoints);
+
+  }
+
+  @Test
+  public void testGetChars() {
+
+    StringBuilder sb = new StringBuilder("abcd𠮷123456");
+    System.out.println("sb = " + sb);
+    char[] copy = new char[20];
+
+    sb.getChars(1, 6, copy, 5);
+
+    System.out.println("Arrays.toString(copy) = " + Arrays.toString(copy));
+
+  }
+
+  @Test
+  public void testAppendNull() {
+
+    StringBuilder sb = new StringBuilder("abcd");
+    System.out.println("sb = " + sb);
+    System.out.println("sb.capacity() = " + sb.capacity());
+    System.out.println("sb.length() = " + sb.length());
+
+    String str = null;
+    sb.append(str);
+    sb.append(str);
+    sb.append(str);
+    sb.append(str);
+    sb.append(str);
+    System.out.println("sb = " + sb);
+    System.out.println("sb.capacity() = " + sb.capacity());
+    System.out.println("sb.length() = " + sb.length());
   }
 
 }
